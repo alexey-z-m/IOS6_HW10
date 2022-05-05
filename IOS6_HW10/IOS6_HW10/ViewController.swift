e891efb8210df216e075268c1469d5fd13a80e19
alexey-z-m <alexey.panda@gmail.com>
2022-05-05 09:35:56 +0300

    comments removed

--- IOS6_HW10/IOS6_HW10/ViewController.swift
+++ IOS6_HW10/IOS6_HW10/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  IOS6_HW10
-//
-//  Created by Panda on 05.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
     func setupView() {
